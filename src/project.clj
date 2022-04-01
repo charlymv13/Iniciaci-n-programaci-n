ec178f0978886729d0507439725444fb94c4f95e
saralugra <saralu1885.slg@gmail.com>
2022-03-31 20:53:39 -0500

    testing push and pull

--- src/project.clj
+++ src/project.clj
@@ -4,3 +4,4 @@
 (def open-NU-list (slurp "resources/TestNU.txt"))
 (def open-ofac-list (slurp "resources/ofac-test.txt"))
 (def open-Dataico-list (slurp "resources/listdataico.xlsx"))
+(def x "hola mundo")
