db12b11855818f886f5659e0bcb49b9a544c78ff
charlymv13 <carlosmendozav98@gmail.com>
2022-03-31 20:30:06 -0500

    read files with slurp function

--- src/project.clj
+++ src/project.clj
@@ -4,15 +4,3 @@
 (def open-NU-list (slurp "resources/TestNU.txt"))
 (def open-ofac-list (slurp "resources/ofac-test.txt"))
 (def open-Dataico-list (slurp "resources/listdataico.xlsx"))
-
-
-open-NU-list
-
-
-(def numeros (list 1 2 3 4 5))
-
-;;funcion   nombre     input   operador
-(defn       suma1     [dato1] (+ dato1 1))
-
-
-(map  suma1 numeros)
